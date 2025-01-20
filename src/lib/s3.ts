6d1e1a0d5fbfa943487187ab3b1059199bfec673
buildingidgr <78495205+buildingidgr@users.noreply.github.com>
2025-01-20 19:00:13 +0200

    sdf

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -2,11 +2,38 @@ import { S3Client } from '@aws-sdk/client-s3';
 import { createPresignedPost } from '@aws-sdk/s3-presigned-post';
 
 const validRegions = [
-  'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2',
-  'eu-west-1', 'eu-west-2', 'eu-west-3',
-  'eu-central-1',
-  'ap-northeast-1', 'ap-northeast-2', 'ap-southeast-1', 'ap-southeast-2',
-  'sa-east-1'
+  // Americas
+  'us-east-1',      // US East (N. Virginia)
+  'us-east-2',      // US East (Ohio)
+  'us-west-1',      // US West (N. California)
+  'us-west-2',      // US West (Oregon)
+  'ca-central-1',   // Canada (Central)
+  'sa-east-1',      // South America (São Paulo)
+  // Europe
+  'eu-west-1',      // Europe (Ireland)
+  'eu-west-2',      // Europe (London)
+  'eu-west-3',      // Europe (Paris)
+  'eu-central-1',   // Europe (Frankfurt)
+  'eu-central-2',   // Europe (Zurich)
+  'eu-south-1',     // Europe (Milan)
+  'eu-south-2',     // Europe (Spain)
+  'eu-north-1',     // Europe (Stockholm)
+  // Asia Pacific
+  'ap-east-1',      // Asia Pacific (Hong Kong)
+  'ap-south-1',     // Asia Pacific (Mumbai)
+  'ap-south-2',     // Asia Pacific (Hyderabad)
+  'ap-southeast-1', // Asia Pacific (Singapore)
+  'ap-southeast-2', // Asia Pacific (Sydney)
+  'ap-southeast-3', // Asia Pacific (Jakarta)
+  'ap-southeast-4', // Asia Pacific (Melbourne)
+  'ap-northeast-1', // Asia Pacific (Tokyo)
+  'ap-northeast-2', // Asia Pacific (Seoul)
+  'ap-northeast-3', // Asia Pacific (Osaka)
+  // Middle East
+  'me-south-1',     // Middle East (Bahrain)
+  'me-central-1',   // Middle East (UAE)
+  // Africa
+  'af-south-1'      // Africa (Cape Town)
 ];
 
 function validateConfig() {
